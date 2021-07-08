994111a785dfac501360b48584bf605afcfec112
Liz Wilson <lizriderwilson@gmail.com>
2021-07-08 13:41:28 -0400

    commit tree df38be1099931865a5504ab26b4e52074f768c97

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+    let stringArray = string.split(' ');
+    const doNotCapitalize = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    stringArray[0] = this.capitalize(stringArray[0]);
+    for (let i = 1; i < stringArray.length; i++) {
+      if (!doNotCapitalize.find(element => element == stringArray[i].toLowerCase())) {
+        stringArray[i] = this.capitalize(stringArray[i]);
+      }
+    }
+    return stringArray.join(' ');
+  }
+
 }
